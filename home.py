baf8619727280d3612535feca4c255eb5f665099
Gabriel Reis <gabrielr.nogueira2000@gmail.com>
2025-08-01 22:41:56 -0300

    removing comments

--- home.py
+++ home.py
@@ -6,16 +6,3 @@ st.set_page_config(page_title="Modelos Compartimentais", layout="centered")
 st.markdown(f'''
         <h1>Modelos compartimentais</h1>
     ''', unsafe_allow_html=True)
-
-# # --- Menu lateral de seleção de modelos ---
-# st.sidebar.title("📊 Escolha o Modelo")
-# model_option = st.sidebar.selectbox(
-#     "Modelo Compartimental",
-#     ["SEIR"]  # Adicione mais aqui no futuro, como "SIR", "SEIRS", etc.
-# )
-
-# # --- Renderização conforme modelo escolhido ---
-# if model_option == "SEIR":
-#     render_seir()
-# else:
-#     st.error("Modelo ainda não implementado.")
